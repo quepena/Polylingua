9f3fb44babfaeb66b47484a83d6d335ebd595a37
quepena <81779884+quepena@users.noreply.github.com>
2021-10-05 22:54:31 +0300

    Add files via upload

---
+++ Web/frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header';
+import './bootstrap.min.css';
+import MessageScreen from './screens/MessageScreen';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MessageScreen />
+    </>
+  );
+}
+
+export default App;
