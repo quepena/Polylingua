d43dbe2865ab4bc7ac01d07cc6fcc05da7be6363
quepena <fishalx6263@gmail.com>
2022-12-22 21:24:28 +0200

    demo user

--- Web/frontend/src/screens/LoginScreen.js
+++ Web/frontend/src/screens/LoginScreen.js
@@ -46,6 +46,7 @@ const LoginScreen = ({ location, history }) => {
                         value={password} onChange={(e) => setPassword(e.target.value)} style={{ marginBottom: "3vh" }}></Form.Control>
                 </Form.Group>
                 <Button variant="primary" type="submit">Sign in</Button>
+                <Button variant="secondary" type="submit" onClick={(e) => {setUsername("demo"); setPassword("demodemo")}}>Sign in as Demo User</Button>
             </Form>
 
             <Row className="py-3">
